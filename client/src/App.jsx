a99743db462d2e372ffbe014da917a73152d87ee
BSofian <draaboufianso@gmail.com>
2024-02-21 16:21:19 +0100

    The chat works on local, with ease and style

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,79 @@
 import './App.css'
+import { useState } from "react"
 
-function App() {
+export default function App({ socket }) {
+
+  const [author, setAuthor] = useState(null)
+  const [messages, setMessages] = useState([])
+
+  const handleConnexion = e => {
+    e.preventDefault()
+
+    socket.on("new_msg", (newMessage) => {
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        newMessage
+      ])
+    })
+
+    const author = e.target.querySelector("#author").value
+    setAuthor(author)
+  }
+
+  const handleSend = e => {
+    e.preventDefault()
+
+    const msg = {
+      content: e.target.querySelector("#message").value,
+      date: Date.now(),
+      author: author
+    }
+
+    socket.emit("send_msg", msg)
+  }
+
+  const msgElements = []
+
+  const myStyle1 = {
+    textAlign: "right",
+    marginLeft: 150
+  }
+
+  messages.map((message, i) => {
+    msgElements.push(
+      author == message.author ?
+        <div key={i} style={myStyle1} >
+          <h2>{message.author}</h2>
+          <p>{message.content}</p>
+          <p><strong>{new Date(message.date).getHours()}h{new Date(message.date).getMinutes()}</strong></p>
+        </div>
+      :
+        <div key={i} >
+          <h2>{message.author}</h2>
+          <p>{message.content}</p>
+          <p><strong>{new Date(message.date).getHours()}h{new Date(message.date).getMinutes()}</strong></p>
+        </div>
+    )
+  }).reverse()
 
   return (
-    <h1>chat app</h1>
+    <div className='app'>
+      <h1>Client Chat</h1>
+      <div className="container">
+        
+        <div>
+          {msgElements}
+        </div>
+      </div>
+      <form onSubmit={handleConnexion} hidden={author != null} >
+        <input type="text" id="author" />
+        <button >Se connecter</button>
+      </form>
+      <form hidden={author == null} onSubmit={handleSend} >
+        <input type="text" id="message" />
+        <button>Envoyer</button>
+      </form>
+    </div>
+
   )
 }
-
-export default App
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// ICI
+import {io} from "socket.io-client"
+const socket = io("localhost:3000")
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App socket={socket} />
   </React.StrictMode>,
 )
